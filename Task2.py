b2e4fda5fc09d2a31dc1cf4e82329690a864485e
Ruchisingh13 <141247708+Ruchisingh13@users.noreply.github.com>
2023-10-28 21:37:54 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,3 +1,4 @@
+# Simple calculator using Python
 print("Select an operation to perform:")
 print("1. Addition")
 print("2. Subtraction")
@@ -6,22 +7,22 @@ print("4. Division")
 
 
 operation = input()
-
+# Addition
 if operation == "1":
     num1 = input("Enter first number:")
     num2 = input("Enter second number:")
     print("The sum is " + str(int(num1) + int(num2)))
-    
+# subtraction    
 elif operation == "2":
     num1 = input("Enter first number:")
     num2 = input("Enter second number:")
     print("The difference is " + str(int(num1) - int(num2)))
-    
+ # Multiplication   
 elif operation == "3":
     num1 = input("Enter first number:")
     num2 = input("Enter second number:")
     print("The product is " + str(int(num1) * int(num2)))
-    
+  # Division  
 elif operation == "4":
     num1 = input("Enter first number:")
     num2 = input("Enter second number:")
