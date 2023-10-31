05e27ce8c24bac2e9c043fceaa3d52a5031d0b7c
Ruchi Singh <141247708+Ruchisingh13@users.noreply.github.com>
2023-10-31 11:52:22 +0530

    Add files via upload

---
+++ Task4.py
@@ -0,0 +1,31 @@
+# Task 4  Rock-Paper- Scissors
+
+import random
+
+while True:
+    user = input("Enter a choice (rock, paper, scissors): ")
+    possibility = ["rock", "paper", "scissors"]
+    computer = random.choice(possibility)
+    print(f"\nYou chose {user}, computer chose {computer}.\n")
+
+    if user == computer:
+        print(f"Both players selected {user}. It's a tie!")
+    elif user == "rock":
+        if computer == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user == "paper":
+        if computer == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user == "scissors":
+        if computer == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
